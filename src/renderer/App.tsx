910eccc724a313f2e453852f6848a8a2412cf404
Ryunosuke Hayashi <ryu525252@gmail.com>
2022-02-25 08:24:25 +0900

    サイドバー作成

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,9 +1,25 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import React, { useEffect, useState  } from 'react';
 import { Button, Card} from 'semantic-ui-react';
-
-
+import './App.css';
 const { myAPI } = window;
+
+const styles : {[key: string]:React.CSSProperties} = {
+  container:{
+      display: 'flex',
+      height: '100vh',
+  },
+  sidebar:{
+      width: '210px',
+      background: '#f9c',
+      overflowY: 'auto',
+  },
+  body:{
+      flex: 1,
+      background: '#9cf',
+      overflowY: 'auto',
+  }
+}
 
 const Home:React.FC = () => {
   const [images1, setImages1] = useState([""]);
@@ -21,11 +37,8 @@ const Home:React.FC = () => {
     };
   },[])
   return (
-    <div>
-      <h1>Diff image</h1>
-      <div><Button onClick={()=>myAPI.openDir("images1")}>OPEN_DIR</Button></div>
-      <div><Button onClick={()=>myAPI.openDir("images2")}>OPEN_DIR</Button></div>
-      <div style={{"width":200}}>
+    <div style={styles.container}>
+      <div style={styles.sidebar}>
       <Card.Group itemsPerRow={2}>
       {images1.map((image,index) => (
         <>
@@ -34,6 +47,11 @@ const Home:React.FC = () => {
         </>
       ))}
       </Card.Group>
+      </div>
+      <div style={styles.body}>
+      <h1>Diff image</h1>
+      <div><Button onClick={()=>myAPI.openDir("images1")}>OPEN_DIR</Button></div>
+      <div><Button onClick={()=>myAPI.openDir("images2")}>OPEN_DIR</Button></div>
       </div>
     </div>
   );
