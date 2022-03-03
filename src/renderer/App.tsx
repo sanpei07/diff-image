917d9ef91040294ca416398152b375cbbd51c5d3
Ryunosuke Hayashi <ryu525252@gmail.com>
2022-03-03 23:16:18 +0900

    画像を中央に表示する等の調整

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,7 +2,6 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import React, { useEffect, useState  } from 'react';
 import { Button, Card ,Segment} from 'semantic-ui-react';
 import './App.css';
-import { callbackify } from 'util';
 const { myAPI } = window;
 
 const styles : {[key: string]:React.CSSProperties} = {
@@ -21,15 +20,31 @@ const styles : {[key: string]:React.CSSProperties} = {
       flex: 1,
       background: '#9cf',
       overflowY: 'auto',
-      //margin:'0 auto',
-      //alignItems: 'center',
-      //justifyContent: 'center',
-      //width: '100vh',
-      // height: '100vh',
+  },
+  bodyImageWrap:{
+    width:'50%',
+    height:'100%',
+    display: 'inline-flex',
+    alignItems: "center",
+    justifyContent:"center",
+    verticalAlign:"middle",
   },
   bodyImage:{
-    width:'50%',
+    width:'100%',
   },
+  dropArea:{
+    width:'70%',
+    height:'70%',
+    border:"1rem solid #aaa",
+    borderRadius:'1rem',
+    fontSize:"3rem",
+    display: 'inline-flex',
+    alignItems: "center",
+    justifyContent:"center",
+    verticalAlign:"middle",
+    cursor: "pointer",
+  },
+
   fixedwrap:{
     //position:'fixed',
     width:'100%',
@@ -54,12 +69,29 @@ const styles : {[key: string]:React.CSSProperties} = {
 }
 
 const Home:React.FC = () => {
-  const [images1, setImages1] = useState([""]);
-  const [images2, setImages2] = useState([""]);
+  const [images1, setImages1] = useState<string[]>([]);
+  const [images2, setImages2] = useState<string[]>([]);
   const [imgNum,setImgNum] = useState(0);
+
+  const openImage1Dir = () =>{
+    myAPI.openDir("images1");
+  }
+  const openImage2Dir = () =>{
+    myAPI.openDir("images2");
+  }
+
+  const imageNext = () =>{
+    (imgNum>0)?setImgNum(imgNum-1):setImgNum(images1.length-1);
+  }
+  const imagePrevious =() =>{
+    (imgNum<images1.length-1)?setImgNum(imgNum+1):setImgNum(0);
+  }
 
   useEffect(()=>{
     myAPI.myPing();
+    ///images1.pop();
+    //setImages1([...images1]);
+    console.log(images1.length);
     const removeListener = myAPI.onReceiveImages((files: string[],msg:string) => {
       if(msg == "images1") setImages1(files);
       if(msg == "images2") setImages2(files);
@@ -69,38 +101,57 @@ const Home:React.FC = () => {
       removeListener();
     };
   },[])
+
   return (
     <div style={styles.container}>
-      <div style={styles.sidebar}>
-      <Card.Group itemsPerRow={2}>
-        <Card><Button icon="folder open outline" onClick={()=>myAPI.openDir("images1")}/></Card>
-        <Card><Button icon="folder open outline" onClick={()=>myAPI.openDir("images2")}></Button></Card>
-      {images1.map((image,index) => (
-        <>
-        <Card raised image={image} onClick={()=>{setImgNum(index)}} />
-        <Card raised image={images2[index] } onClick={()=>{setImgNum(index)}} />
-        </>
-      ))}
-      </Card.Group>
-      </div>
+      
       <div style={styles.body}>
-        <img src={images1[imgNum]} style={styles.bodyImage} ></img>
-        <img src={images2[imgNum] } style={styles.bodyImage}></img>
+
+        <div style={styles.bodyImageWrap}>
+          {(images1.length > 0)? 
+            (<img src={images1[imgNum]} style={styles.bodyImage} ></img>):
+            (<div style={styles.dropArea} onClick={()=>{openImage1Dir()}}>FOLDER1</div>)}
+        </div>
+        <div style={styles.bodyImageWrap}>
+          {(images2.length > 0)? 
+            (<img src={images2[imgNum]} style={styles.bodyImage} ></img>):
+            (<div style={styles.dropArea} onClick={()=>{openImage2Dir()}}>FOLDER2</div>)}
+        </div>
+
         <div style={styles.fixedwrap}><div style={styles.editbar}>
-        <Segment>
-        <Button icon="arrow alternate circle left outline" style={styles.editbutton} onClick={()=>{(imgNum>0)?setImgNum(imgNum-1):setImgNum(images1.length-1)}}></Button>
-        <Button icon="arrow alternate circle right outline" style={styles.editbutton} onClick={()=>{(imgNum<images1.length-1)?setImgNum(imgNum+1):setImgNum(0)}}></Button>
-        <Button icon="trash alternate outline" style={styles.editbutton} onClick={()=>{
-          myAPI.deleteImage(images1[imgNum],images2[imgNum]);
-          images1.splice(imgNum,1);
-          images2.splice(imgNum,1);
-          setImages1([...images1]);
-          setImages2([...images2]);
-          }}></Button>
+          <Segment>
+            <Button icon="arrow alternate circle left outline" style={styles.editbutton} onClick={()=>{imageNext()}}></Button>
+            <Button icon="arrow alternate circle right outline" style={styles.editbutton} onClick={()=>{imagePrevious()}}></Button>
+            <Button icon="trash alternate outline" style={styles.editbutton} onClick={()=>{
+              myAPI.deleteImage(images1[imgNum],images2[imgNum]);
+              images1.splice(imgNum,1);
+              images2.splice(imgNum,1);
+              setImages1([...images1]);
+              setImages2([...images2]);
+              }}></Button>
           </Segment>
-          </div>
-          </div>
+        </div>
       </div>
+      </div>
+
+      <div style={styles.sidebar}>
+        <Card.Group itemsPerRow={2}>
+          {/*<Card><Button icon="folder open outline" onClick={()=>myAPI.openDir("images1")}/></Card>
+          <Card><Button icon="folder open outline" onClick={()=>myAPI.openDir("images2")}/></Card>*/}
+          {(images1.length>images2.length)?(images1.map((image,index) => (
+          <>
+          <Card raised image={image} onClick={()=>{setImgNum(index)}} />
+          <Card raised image={images2[index] } onClick={()=>{setImgNum(index)}} />
+          </>
+          ))):(images2.map((image,index) => (
+            <>
+            <Card raised image={images1[index]} onClick={()=>{setImgNum(index)}} />
+            <Card raised image={image} onClick={()=>{setImgNum(index)}} />
+            </>
+            )))}
+        </Card.Group>
+      </div>
+
     </div>
   );
 };
